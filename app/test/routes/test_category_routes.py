43a0e9afe96bb5fb050a642ce7edec30be233605
Rafael <rjribeiro@inf.ufrgs.br>
2023-07-10 14:58:21 -0300

    list categories route

--- app/test/routes/test_category_routes.py
+++ app/test/routes/test_category_routes.py
@@ -5,6 +5,7 @@ from typing import Any
 
 from app.main import app
 from app.db.models import CategoryModel
+from app.schemas.category import CategoryOutputSchema
 
 client = TestClient(app)
 
@@ -22,3 +23,13 @@ def test_add_category_route(db_session: Session, body_roupa: dict[str, Any]):
     assert category_on_db[0].slug == body_roupa['slug']
     db_session.delete(category_on_db[0])
     db_session.commit()
+
+def test_list_categories_routes(db_session: Session, categories_on_db: list[CategoryModel]):
+    response = client.get('/category/list')
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert len(data) == 3
+    assert data[0] == CategoryOutputSchema(**categories_on_db[0].__dict__).model_dump()
+    assert data[1] == CategoryOutputSchema(**categories_on_db[1].__dict__).model_dump()
+    assert data[2] == CategoryOutputSchema(**categories_on_db[2].__dict__).model_dump()
+
